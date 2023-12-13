f50ae8e5ce2014ddfa8f9408c97c4e08dccff9af
John Arocha Dev <jparocha777@gmail.com>
2023-12-13 15:10:24 -0500

    flight/new works

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,5 +1,9 @@
 const Flight = require('../models/flight')
 
 module.exports = {
-    
+    new : newFlight,
+}
+
+function newFlight(req, res) {
+    res.render('flights/new', {errorMsg: ' '})
 }
--- models/flight.js
+++ models/flight.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
+
 const Schema = mongoose.Schema
+
 const flightSchema = new Schema({
     airline: String,
     airport: String,
