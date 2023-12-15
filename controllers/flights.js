b3188361b14fecc0f049a91aa3a41acadea94950
John Arocha Dev <jparocha777@gmail.com>
2023-12-15 18:21:28 -0500

    flights and add work

--- controllers/flights.js
+++ controllers/flights.js
@@ -2,8 +2,20 @@ const Flight = require('../models/flight')
 
 module.exports = {
     new : newFlight,
+    index,
 }
 
 function newFlight(req, res) {
     res.render('flights/new', {errorMsg: ' '})
+}
+
+function getAll() {
+    return Flight;
+}
+
+function index(req, res) {
+    res.render('flights/index', {
+        flights: getAll(),
+        title: 'Open FLights'
+    })
 }
--- routes/flights.js
+++ routes/flights.js
@@ -4,11 +4,10 @@ const router = express.Router();
 
 const moviesCtrl = require('../controllers/flights')
 
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+router.get('/', moviesCtrl.index)
 
 router.get('/new', moviesCtrl.new)
+
+
 
 module.exports = router;
