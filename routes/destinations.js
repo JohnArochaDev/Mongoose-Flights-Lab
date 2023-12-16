360a79e2bec56d9044a1bcc08a7d0a75dee3db53
John Arocha Dev <jparocha777@gmail.com>
2023-12-15 21:44:32 -0500

    fixed destinations router

--- routes/destinations.js
+++ routes/destinations.js
@@ -2,3 +2,8 @@ const express = require('express');
 const router = express.Router();
 const destCtrl = require('../controllers/destinations')
 
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
