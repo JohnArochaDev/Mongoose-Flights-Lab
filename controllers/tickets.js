fc6481a394326d32affeff5d4bbbe991d1462eac
John Arocha Dev <jparocha777@gmail.com>
2023-12-16 01:18:41 -0500

    need sleep

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -3,11 +3,17 @@ const Tickets = require('../models/ticket')
 
 module.exports = {
     create,
+    show,
+}
+
+async function show(req, res) {
+    const flight = await Flight.findById(req.params.id);
+    res.render(`/tickets/new`)
 }
 
 async function create(req, res) {
     const flight = await Flight.findById(req.params.id)
     flight.seat.push(req.body)
     await flight.save()
-    res.redirect(`/flights/${flight._id}`)
+    res.redirect(`/flights/${flight._id}/tickets/new`)
 };
--- routes/tickets.js
+++ routes/tickets.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const ticketCtrl = require('../controllers/tickets')
 
+router.get('/:id/tickets/new', ticketCtrl.show)
+
 router.post('/:id/tickets/new', ticketCtrl.create);
 
 module.exports = router;
