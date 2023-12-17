8b921931d44fb6d7ad0168d35ca63a8f1aade03f
John Arocha Dev <jparocha777@gmail.com>
2023-12-17 11:27:06 -0500

    almost done

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,5 +1,5 @@
 const Flight = require('../models/flight')
-const Tickets = require('../models/ticket')
+const Ticket = require('../models/ticket')
 
 module.exports = {
     create,
@@ -8,13 +8,13 @@ module.exports = {
 
 async function show(req, res) {
     const flight = await Flight.findById(req.params.id);
-    res.render(`flights/tickets/new`, { flight })
+    const ticket = await Ticket.findById(req.params.id);
+    res.render(`flights/tickets/new`, { ticket, flight })
 }
 
 async function create(req, res) {
-    const ticket = await Tickets.findById(req.params.id)
     const flight = await Flight.findById(req.params.id)
-    // ticket.push(req.body)
-    // await ticket.save()
-    res.redirect(`/flights/${flight._id}/tickets/new`, { ticket, flight })
+    req.body.flight = flight
+    await Ticket.create(req.body)
+    res.redirect(`/flights/${flight._id}`)
 };
